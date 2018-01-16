b50ccdf3643f47dffa546aa67205f4f66503e124
dpjude <dpjude@gmail.com>
2018-01-16 23:31:40 +0800

    change redis ip to gcp

--- autocomplete.py
+++ autocomplete.py
@@ -4,7 +4,7 @@ import uuid
 import random
 import redis
 
-redishost = '192.168.56.2'
+redishost = '10.146.0.3'
 redisport = '6379'
 conn0 = redis.StrictRedis(host=redishost, port=redisport, db=0)
 conn1 = redis.StrictRedis(host=redishost, port=redisport, db=1)
